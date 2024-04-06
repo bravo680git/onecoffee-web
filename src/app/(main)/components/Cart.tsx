92b93247918edc96a5f7980c9695e8de3d5a86b6
bravo680 <iambravo680@gmail.com>
2024-04-06 15:34:15 +0700

    create cart component

---
+++ src/app/(main)/components/Cart.tsx
@@ -0,0 +1,186 @@
+"use client";
+
+import Checkbox from "@/components/Checkbox";
+import clsx from "clsx";
+import {
+  Add,
+  ArrowLeft,
+  CloseSquare,
+  Minus,
+  ShoppingCart,
+} from "iconsax-react";
+import Image from "next/image";
+import { useState, useEffect, MouseEvent } from "react";
+import { createPortal } from "react-dom";
+
+function Cart() {
+  const [mounted, setMounted] = useState(false);
+  const [open, setOpen] = useState(false);
+  const items = [1, 2, 3, 4];
+  const [selectedItems, setSelectedItems] = useState<number[]>([]);
+
+  const handleSelectItem = (v: number, all = false) => {
+    if (all) {
+      if (selectedItems.length === items.length) {
+        setSelectedItems([]);
+      } else {
+        setSelectedItems([...items]);
+      }
+      return;
+    }
+    if (selectedItems.includes(v)) {
+      setSelectedItems(selectedItems.filter((i) => i !== v));
+    } else {
+      setSelectedItems([...selectedItems, v]);
+    }
+  };
+
+  useEffect(() => {
+    setMounted(true);
+
+    const handleClose = () => {
+      setOpen(false);
+    };
+
+    window.addEventListener("click", handleClose);
+
+    return () => {
+      window.removeEventListener("click", handleClose);
+    };
+  }, []);
+
+  return (
+    <>
+      <CartBtn
+        onClick={(e) => {
+          e.stopPropagation();
+          setOpen(true);
+        }}
+      />
+      {mounted &&
+        createPortal(
+          <div
+            className={clsx(
+              "invisible fixed bottom-0 left-0 right-0 top-0 z-20 bg-black/20 transition-all",
+              {
+                "!visible": open,
+              },
+            )}
+            data-dialog-open={open}>
+            <div
+              className={clsx(
+                "fixed right-0 top-0 h-full w-0 overflow-hidden bg-white opacity-0 transition-all duration-300",
+                {
+                  "w-[500px] !opacity-100 sm:w-full": open,
+                },
+              )}
+              onClick={(e) => e.stopPropagation()}>
+              <div className="flex items-center gap-2 p-4 shadow-md">
+                <button
+                  className="ripple rounded-md text-neutral-text-secondary hover:opacity-80"
+                  onClick={() => setOpen(false)}>
+                  <ArrowLeft size={20} />
+                </button>
+                <h3 className="text-lg font-bold">Giỏ hàng</h3>
+              </div>
+              <div className="flex h-[calc(100vh-176px)] flex-col gap-4 overflow-y-auto p-4">
+                {[1, 2, 3, 4].map((i) => (
+                  <CartItem
+                    key={i}
+                    checked={selectedItems.includes(i)}
+                    onCheckedChange={() => handleSelectItem(i)}
+                  />
+                ))}
+              </div>
+              <div className="p-4 shadow-[0_-4px_6px_-1px_rgba(0,0,0,0.1)]">
+                <div className="flex items-center justify-between">
+                  <Checkbox
+                    value={selectedItems.length === items.length}
+                    onChange={() => handleSelectItem(0, true)}
+                    label="Tất cả"
+                  />
+                  <div className="flex items-center gap-1">
+                    <span className="text-neutral-text-secondary">
+                      Tổng thanh toán
+                    </span>
+                    <span className="text-lg font-bold text-primary-500">
+                      &#8363;120000
+                    </span>
+                  </div>
+                </div>
+                <button
+                  className="ripple mt-4 flex w-full items-center justify-center gap-2 rounded-md
+                  bg-primary-500 py-2 font-semibold text-white transition-all duration-300 active:shadow-primary">
+                  <ShoppingCart />
+                  Mua hàng
+                </button>
+              </div>
+            </div>
+          </div>,
+          document.body,
+        )}
+    </>
+  );
+}
+
+type CartItemProps = {
+  checked: boolean;
+  onCheckedChange(): void;
+};
+
+function CartItem({ checked, onCheckedChange }: CartItemProps) {
+  return (
+    <div className="flex items-start gap-4 border-b border-slate-200 pb-4">
+      <div className="flex shrink-0 items-center gap-1">
+        <div className="shrink-0">
+          <Checkbox value={checked} onChange={onCheckedChange} />
+        </div>
+        <div className="relative h-20 w-20 shrink-0">
+          <Image src="/images/product/apple.png" alt="" fill />
+        </div>
+      </div>
+      <div className="flex flex-col gap-1">
+        <h4 className="line-clamp-1 overflow-hidden text-ellipsis text-lg font-semibold">
+          Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.
+        </h4>
+        <span className="text-neutral-text-secondary">Phân loại: Đen, XL</span>
+        <div className="flex gap-2">
+          <span className="font-semibold text-primary-500">&#8363;130.000</span>
+          <span className="text-neutral-text-secondary line-through">
+            &#8363;150.000
+          </span>
+        </div>
+        <div className="mt-2 flex h-8 w-fit items-stretch divide-x overflow-hidden rounded-md border border-slate-200">
+          <i className="ripple flex cursor-pointer items-center justify-center px-2 text-neutral-text-secondary">
+            <Minus size={16} />
+          </i>
+          <span className="px-3 py-1 font-semibold">2</span>
+          <i className="ripple flex cursor-pointer items-center justify-center px-2 text-neutral-text-secondary">
+            <Add size={16} />
+          </i>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function CartBtn({
+  onClick,
+}: {
+  onClick(e: MouseEvent<HTMLDivElement>): void;
+}) {
+  return (
+    <div
+      className="relative cursor-pointer transition-all hover:text-primary-500"
+      onClick={onClick}>
+      <ShoppingCart size={24} />
+      <div
+        className="absolute right-[-8px] top-[-8px] flex h-4 w-4 items-center
+         justify-center rounded-full bg-primary-500 text-[10px] font-bold text-white">
+        5
+      </div>
+    </div>
+  );
+}
+
+export default Cart;
--- src/app/(main)/components/Header.tsx
+++ src/app/(main)/components/Header.tsx
@@ -1,15 +1,10 @@
 "use client";
 
-import { useState, useEffect, MouseEvent, useRef } from "react";
-import {
-  Add,
-  ArrowDown2,
-  HambergerMenu,
-  SearchNormal1,
-  ShoppingCart,
-} from "iconsax-react";
-import Image from "next/image";
 import clsx from "clsx";
+import { Add, ArrowDown2, HambergerMenu, SearchNormal1 } from "iconsax-react";
+import Image from "next/image";
+import { MouseEvent, useEffect, useRef, useState } from "react";
+import Cart from "./Cart";
 
 export type MenuItem = {
   key: string;
@@ -63,40 +58,40 @@ function Header({ menuItems }: { menuItems: MenuItem[] }) {
     <header>
       <div
         className={clsx(
-          "flex h-32 px-10 items-center border-slate-100 justify-between fixed top-0 left-0 w-full z-20 text-white transition-all md:hidden",
+          "fixed left-0 top-0 z-20 flex h-32 w-full items-center justify-between border-slate-100 px-10 text-white transition-all md:hidden",
           {
-            "bg-neutral-bg-footer/60 !h-20": !onInitScroll,
-          }
+            "!h-20 bg-neutral-bg-footer/60": !onInitScroll,
+          },
         )}>
-        <div className="h-16 flex items-center gap-2 w-64 xl:w-16">
+        <div className="flex h-16 w-64 items-center gap-2 xl:w-16">
           <Image src="/logo.png" width={64} height={64} alt="" />
           <h2 className="text-lg font-bold xl:invisible">New3T store</h2>
         </div>
 
-        <ul className="flex gap-4 items-center justify-self-center font-semibold">
+        <ul className="flex items-center gap-4 justify-self-center font-semibold">
           {menuItems.map((item, index) => (
             <li
               key={index}
-              className="cursor-pointer relative group flex items-center gap-1 transition-all hover:bg-primary-500/20 py-1 px-2 rounded-md">
+              className="group relative flex cursor-pointer items-center gap-1 rounded-md px-2 py-1 transition-all hover:bg-primary-500/20">
               {item.title}
               {!!item.children?.length && (
                 <ArrowDown2
-                  className="group-hover:rotate-180 transition-all"
+                  className="transition-all group-hover:rotate-180"
                   size={16}
                 />
               )}
               <ul
                 className={clsx(
-                  "absolute shadow-lg left-[50%] translate-x-[-50%] top-[calc(100%+4px)] invisible",
-                  "group-hover:visible transition-all z-10 rounded-md scale-75 group-hover:scale-100",
+                  "invisible absolute left-[50%] top-[calc(100%+4px)] translate-x-[-50%] shadow-lg",
+                  "z-10 scale-75 rounded-md transition-all group-hover:visible group-hover:scale-100",
                   {
                     "bg-neutral-bg-footer/60": !onInitScroll,
-                  }
+                  },
                 )}>
                 {item.children?.map((child, i) => (
                   <li
                     key={i}
-                    className="whitespace-nowrap px-4 py-2 cursor-pointer hover:bg-primary-500/20 rounded-md transition-all">
+                    className="cursor-pointer whitespace-nowrap rounded-md px-4 py-2 transition-all hover:bg-primary-500/20">
                     {child.title}
                   </li>
                 ))}
@@ -105,30 +100,30 @@ function Header({ menuItems }: { menuItems: MenuItem[] }) {
           ))}
         </ul>
 
-        <div className="flex gap-5 text-neutral-text-secondary w-64 justify-end xl:w-fit">
-          <div className="cursor-pointer hover:text-primary-500 transition-all flex items-center">
+        <div className="flex w-64 justify-end gap-5 text-neutral-text-secondary xl:w-fit">
+          <div className="flex cursor-pointer items-center transition-all hover:text-primary-500">
             <input
               ref={inputRef}
               placeholder="Tìm kiếm gì đó..."
               className={clsx(
-                "border-b-2 border-primary-500 caret-primary-500 transition-all w-0 bg-transparent text-white",
+                "w-0 border-b-2 border-primary-500 bg-transparent text-white caret-primary-500 transition-all",
                 {
                   "!w-40": searching,
-                }
+                },
               )}
             />
             <SearchNormal1 size={24} onClick={handleSearch} />
           </div>
-          <CartBtn />
+          <Cart />
         </div>
       </div>
       <div
         className={clsx(
-          "hidden md:flex h-14 p-4 items-center justify-between text-neutral-text-secondary",
-          "transition-all fixed w-full z-10",
+          "hidden h-14 items-center justify-between p-4 text-neutral-text-secondary md:flex",
+          "fixed z-10 w-full transition-all",
           {
             "bg-neutral-bg-footer/90": !onInitScroll,
-          }
+          },
         )}>
         <div>
           <HambergerMenu size={20} onClick={() => setOpen(true)} />
@@ -139,31 +134,31 @@ function Header({ menuItems }: { menuItems: MenuItem[] }) {
               ref={inputRef}
               placeholder="Tìm kiếm gì đó..."
               className={clsx(
-                "border-b-2 border-primary-500 caret-primary-500 transition-all w-0 bg-transparent text-white",
+                "w-0 border-b-2 border-primary-500 bg-transparent text-white caret-primary-500 transition-all",
                 {
                   "!w-40": searching,
-                }
+                },
               )}
             />
             <SearchNormal1 size={20} onClick={handleSearch} />
           </div>
-          <CartBtn />
+          <Cart />
         </div>
         <div
           className={clsx(
-            "h-screen fixed z-10 left-0  w-screen bg-black/20 top-0 invisible overflow-hidden transition-all",
+            "invisible fixed left-0 top-0  z-10 h-screen w-screen overflow-hidden bg-black/20 transition-all",
             {
               "!visible": open,
-            }
+            },
           )}
           onClick={() => setOpen(false)}>
           <div
             className={clsx(
-              "w-0 bg-neutral-bg-footer/90 text-white h-full p-4 transition-all duration-300",
-              "overflow-x-hidden overflow-y-auto relative opacity-0 whitespace-nowrap",
+              "h-full w-0 bg-neutral-bg-footer/90 p-4 text-white transition-all duration-300",
+              "relative overflow-y-auto overflow-x-hidden whitespace-nowrap opacity-0",
               {
                 "!w-[300px] !opacity-100": open,
-              }
+              },
             )}>
             <Add
               size={32}
@@ -174,10 +169,10 @@ function Header({ menuItems }: { menuItems: MenuItem[] }) {
               <Image src="/logo.png" width={60} height={60} alt="" />
               <h2 className="text-lg font-bold">New3T store</h2>
             </div>
-            <ul className="flex flex-col font-semibold mt-8 gap-2">
+            <ul className="mt-8 flex flex-col gap-2 font-semibold">
               {menuItems.map((item, index) => (
                 <li key={index} className="cursor-pointer">
-                  <span className="flex items-center gap-1 hover:bg-primary-500/20 py-1 px-2 rounded-md transition-all">
+                  <span className="flex items-center gap-1 rounded-md px-2 py-1 transition-all hover:bg-primary-500/20">
                     {item.title}
                     {!!item.children?.length && (
                       <ArrowDown2
@@ -191,7 +186,7 @@ function Header({ menuItems }: { menuItems: MenuItem[] }) {
                   </span>
                   <ul
                     className={clsx(
-                      "transition-all duration-300 overflow-hidden h-0"
+                      "h-0 overflow-hidden transition-all duration-300",
                     )}
                     style={{
                       height: expandItems.includes(item.key)
@@ -201,7 +196,7 @@ function Header({ menuItems }: { menuItems: MenuItem[] }) {
                     {item.children?.map((child, i) => (
                       <li
                         key={i}
-                        className="whitespace-nowrap px-4 py-2 cursor-pointer hover:bg-primary-500/20 rounded-md transition-all">
+                        className="cursor-pointer whitespace-nowrap rounded-md px-4 py-2 transition-all hover:bg-primary-500/20">
                         {child.title}
                       </li>
                     ))}
@@ -213,19 +208,6 @@ function Header({ menuItems }: { menuItems: MenuItem[] }) {
         </div>
       </div>
     </header>
-  );
-}
-
-function CartBtn() {
-  return (
-    <div className="relative cursor-pointer hover:text-primary-500 transition-all">
-      <ShoppingCart size={24} />
-      <div
-        className="absolute w-4 h-4 text-[10px] flex items-center justify-center
-         bg-primary-500 text-white font-bold rounded-full top-[-8px] right-[-8px]">
-        5
-      </div>
-    </div>
   );
 }
 
