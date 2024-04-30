77ede5e87d4a88fb1e523d0c2092257c1c2f506d
bravo680 <iambravo680@gmail.com>
2024-04-30 17:25:47 +0700

    add cors for api revalidate

--- environment.d.ts
+++ environment.d.ts
@@ -2,5 +2,6 @@ declare namespace NodeJS {
   export interface ProcessEnv {
     readonly BASE_URL: string;
     readonly NEXT_PUBLIC_GG_CLIENT_ID: string;
+    readonly ADMIN_DOMAIN: string;
   }
 }
--- src/app/api/revalidate-tag/[tag]/route.ts
+++ src/app/api/revalidate-tag/[tag]/route.ts
@@ -1,15 +1,36 @@
 import { revalidateTag } from "next/cache";
 
-export async function GET(
+export async function POST(
   req: Request,
   { params }: { params: { tag: string } },
 ) {
   const { tag } = params;
   revalidateTag(tag);
 
-  return Response.json({
-    status: true,
-    tag,
-    timestamp: new Date().toISOString(),
+  return Response.json(
+    {
+      status: true,
+      tag,
+      timestamp: new Date().toISOString(),
+    },
+    {
+      status: 200,
+      headers: {
+        "Access-Control-Allow-Origin": process.env.ADMIN_DOMAIN,
+        "Access-Control-Allow-Methods": "POST, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type, token",
+      },
+    },
+  );
+}
+
+export async function OPTIONS() {
+  return new Response(null, {
+    status: 204,
+    headers: {
+      "Access-Control-Allow-Origin": process.env.ADMIN_DOMAIN,
+      "Access-Control-Allow-Methods": "POST, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type, token",
+    },
   });
 }
