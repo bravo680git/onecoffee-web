5300c8a4bb2c5f37e4431b54a0ce9dc51b4cd412
bravo680 <85890426+bravo680git@users.noreply.github.com>
2024-06-27 15:47:22 +0700

    Fix err: use className <- class in Loading component

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -2,20 +2,20 @@ import './Loading.css'
 
 function Loading() {
 
-    return <div class="loading-window">
-        <div class="car">
-            <div class="strike"></div>
-            <div class="strike strike2"></div>
-            <div class="strike strike3"></div>
-            <div class="strike strike4"></div>
-            <div class="strike strike5"></div>
-            <div class="car-detail spoiler"></div>
-            <div class="car-detail back"></div>
-            <div class="car-detail center"></div>
-            <div class="car-detail center1"></div>
-            <div class="car-detail front"></div>
-            <div class="car-detail wheel"></div>
-            <div class="car-detail wheel wheel2"></div>
+    return <div className="loading-window">
+        <div className="car">
+            <div className="strike"></div>
+            <div className="strike strike2"></div>
+            <div className="strike strike3"></div>
+            <div className="strike strike4"></div>
+            <div className="strike strike5"></div>
+            <div className="car-detail spoiler"></div>
+            <div className="car-detail back"></div>
+            <div className="car-detail center"></div>
+            <div className="car-detail center1"></div>
+            <div className="car-detail front"></div>
+            <div className="car-detail wheel"></div>
+            <div className="car-detail wheel wheel2"></div>
         </div>
     </div>
 }
